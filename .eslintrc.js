bf239ff9b90f760b7958494bbad24eca86689f25
Tiago Nunes <tiagonunes.developer@gmail.com>
2017-04-26 16:02:16 -0300

    commit dos testes Generic Search closed #3

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,18 @@
 module.exports = {
-    "extends": "airbnb",
-    "plugins": [
-        "react",
-        "jsx-a11y",
-        "import"
-    ]
+  "extends": "eslint:recommended",
+  "plugins": [
+    "react", "jsx-a11y", "import"
+  ],
+  "env": {
+    "node": true,
+    "mocha": true,
+    "browser": true
+  },
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module"
+  },
+  "rules": {
+    "semi": 0
+  }
 }
